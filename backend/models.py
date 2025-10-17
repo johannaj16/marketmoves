829c5f0968f3660046c70f4e59edd1de207b94ed
Mallika Pamula <pmallika@vt.edu>
2025-10-17 13:59:09 -0400

    Database model

--- backend/models.py
+++ backend/models.py
@@ -1,17 +1,34 @@
-from pydantic import BaseModel, PositiveInt
+from pydantic import BaseModel, PositiveInt, Field
 from supabase_auth import Optional
 
 
 class User(BaseModel):
-    id: Optional[int] # primary_key needs to be unique and will increment automatically 
+    user_id: Optional[int] = None   # primary_key needs to be unique and will increment automatically 
     email: str
-    firstName: str
-    lastName: str
-    balance: float = 10000.0  # default value 
+    first_name: str
+    last_name: str
+    balance: float = 10000.0        # default value 
 
 class Order(BaseModel):
-    id: Optional[int]
-    user_ID: int
+    order_id: Optional[int] = None  # auto increment
+    user_id: int
     stock: str
     quantity: PositiveInt 
-    transaction: str            # buy or sell
+    price: int                  # at the time of transaction
+    order_type: str            # buy or sell
+
+class Watchlist(BaseModel):
+    user_id: int = Field(primary_key=True)
+    name: str = Field(primary_key=True)
+
+    
+class Symbol(BaseModel):
+    ticker: str = Field(primary_key=True)      # ex: AAPL
+    name: str           # ex: Apple Inc.
+    exchange: str       # ex: NASDAQ
+
+class WatchlistSymbol(BaseModel):
+    user_id: int = Field(primary_key=True)
+    name: str = Field(primary_key=True)
+    ticker: str         # ex: AAPL 
+
--- backend/user_routes.py
+++ backend/user_routes.py
@@ -14,7 +14,7 @@ async def ping():
 def get_all_users():
     try: 
         response = supabase.table("User").select("*").execute()
-        users = response.data
+        users = User(**response.data)
     except Exception as e: 
         raise HTTPException(status_code=500, detail=f"Database session connection issue: {e}\n")
     return users   # query using ORM model
@@ -31,3 +31,12 @@ def get_user(id: int):
         raise HTTPException(status_code=404, detail='User cannot be found\n')
         
     return user[0]
+
+
+@router.delete("/menu/deleteUser/{id}")
+def delete_user(id: int):
+    try:
+        response = supabase.table("User").delete().eq("id", id)
+    except Exception as e: 
+        raise HTTPException(status_code=500, detail=f"Database session connection issue: {e}\n")
+        
